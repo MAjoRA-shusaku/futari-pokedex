de2e119d72d00549cf98318e5918d0bb3df9602c
MAjoRA-shusaku <shusaku7340120@gmail.com>
2026-08-23 23:37:41 +0900

    Add files via upload

---
+++ outputs/config.js
@@ -0,0 +1,2 @@
+// anon key はブラウザ用の公開キーです。service_role key は絶対に入れないでください。
+window.FUTARI_DEX_CONFIG = { supabaseUrl: 'https://gmjtfuoahwxcpkphbqva.supabase.co', supabaseAnonKey: 'sb_publishable_THlsCqAgckgUHzpyoh8zGw_3skC30RO', room: 'aika-shusaku-2026', members: 'あいか & しゅうさく' };
